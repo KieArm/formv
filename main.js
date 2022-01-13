8953245d75f447755115ab3811b74a9b4191f982
KieArm <87212808+KieArm@users.noreply.github.com>
2022-01-13 12:28:53 +0800

    v1

---
+++ main.js
@@ -0,0 +1,63 @@
+const form = document.getElementById('form')
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const passwordCheck = document.getElementById('passwordcheck')
+
+form.addEventListener('submit' , (e) => {
+    e.preventDefault();
+    checkInputs();
+})
+
+function checkInputs() {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const passwordCheckValue = passwordCheck.value.trim();
+
+    if(usernameValue === '') {
+        setErrorFor(username, 'Username cannot be blank');
+    } else {
+        setSuccesFor(username);
+    }
+
+    if(emailValue === '') {
+        setErrorFor(email, 'Email cannot be blank');
+    } else if(!isEmail(emailValue)) {
+        setErrorFor(email, 'Email is not valid');
+    } else {
+        setSuccesFor(email);
+    }
+
+    if(passwordValue === '') {
+        setErrorFor(password, 'Password cannot be blank');
+    } else {
+        setSuccesFor(password);
+    }
+
+    if(passwordCheckValue === '') {
+        setErrorFor(passwordcheck, 'Password cannot be blank');
+    } else if(passwordValue !== passwordCheckValue) {
+        setErrorFor(passwordcheck, 'Passwords do not match');
+    } else {
+        setSuccesFor(passwordcheck);
+    }
+}
+
+function setErrorFor (input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+
+    small.innerText = message;
+
+    formControl.className = 'form-control error';
+}
+
+function setSuccesFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+} // thanks google :)
